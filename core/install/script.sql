85b745d51d05bc7c48b26b72d040e0ff8cf7a4d5
Duy Trần <duytran.bkict@gmail.com>
2014-04-22 10:31:45 +0700

    Init commit

---
+++ core/install/script.sql
@@ -0,0 +1,153 @@
+DROP DATABASE IF EXISTS travel_agency; 
+CREATE DATABASE travel_agency DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE travel_agency;
+
+CREATE TABLE destinations (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL,
+	description VARCHAR(1024) NOT NULL,
+	published BIT NOT NULL DEFAULT 1,
+	published_date DATETIME,
+	created_date DATETIME,
+	modified_date DATETIME
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+CREATE TABLE tours (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL,
+	price float NOT NULL,
+	short_description VARCHAR(512),
+	description TEXT,
+	destination_id INT REFERENCES destinations(id),
+	published BIT NOT NULL DEFAULT 1,
+	published_date DATETIME,
+	created_date DATETIME,
+	modified_date DATETIME
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+CREATE TABLE users (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(128) NOT NULL,
+	password VARCHAR(128) NOT NULL,
+	role VARCHAR(16) NOT NULL DEFAULT 'guest',
+	created_date DATETIME,
+	modified_date DATETIME
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+CREATE TABLE orders (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	user_id INT,
+	status INT DEFAULT 1, -- 1: pending, 2: confirmed, 3: canceled
+	created_date DATETIME,
+	modified_date DATETIME
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+CREATE TABLE order_items (
+	item_id INT NOT NULL REFERENCES users(id),
+	order_id INT NOT NULL REFERENCES orders(id),
+	quantity INT NOT NULL CHECK (quantity > 0),
+	price float NOT NULL,
+	PRIMARY KEY (item_id, order_id)
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+DROP TRIGGER IF EXISTS tgg_insert_destinations;
+delimiter //
+CREATE TRIGGER tgg_insert_destinations 
+BEFORE INSERT ON destinations
+FOR EACH ROW 
+BEGIN
+	SET NEW.created_date = now();
+	SET NEW.modified_date = now();
+	IF NEW.published = 1 THEN
+		SET NEW.published_date = now();
+	END IF;
+END;//
+delimiter ;
+
+DROP TRIGGER IF EXISTS tgg_update_destinations;
+delimiter //
+CREATE TRIGGER tgg_update_destinations 
+BEFORE UPDATE ON destinations
+FOR EACH ROW 
+BEGIN
+	SET NEW.modified_date = now();
+	IF NEW.published != OLD.published AND NEW.published = 1 THEN
+		SET NEW.published_date = now();
+	END IF;
+END;//
+delimiter ;
+
+DROP TRIGGER IF EXISTS tgg_insert_tours;
+delimiter //
+CREATE TRIGGER tgg_insert_tours
+BEFORE INSERT ON tours
+FOR EACH ROW 
+BEGIN
+	SET NEW.created_date = now();
+	SET NEW.modified_date = now();
+	IF NEW.published = 1 THEN
+		SET NEW.published_date = now();
+	END IF;
+END;//
+delimiter ;
+
+DROP TRIGGER IF EXISTS tgg_update_tours;
+delimiter //
+CREATE TRIGGER tgg_update_tours
+BEFORE UPDATE ON tours
+FOR EACH ROW 
+BEGIN
+	SET NEW.modified_date = now();
+	IF NEW.published != OLD.published AND NEW.published = 1 THEN
+		SET NEW.published_date = now();
+	END IF;
+END;//
+delimiter ;
+
+DROP TRIGGER IF EXISTS tgg_insert_users;
+delimiter //
+CREATE TRIGGER tgg_insert_users
+BEFORE INSERT ON users
+FOR EACH ROW 
+BEGIN
+	SET NEW.created_date = now();
+	SET NEW.modified_date = now();
+END;//
+delimiter ;
+
+DROP TRIGGER IF EXISTS tgg_update_users;
+delimiter //
+CREATE TRIGGER tgg_update_users
+BEFORE UPDATE ON users
+FOR EACH ROW 
+BEGIN
+	SET NEW.modified_date = now();
+END;//
+delimiter ;
+
+DROP TRIGGER IF EXISTS tgg_insert_orders;
+delimiter //
+CREATE TRIGGER tgg_insert_orders
+BEFORE INSERT ON orders
+FOR EACH ROW 
+BEGIN
+	SET NEW.created_date = now();
+	SET NEW.modified_date = now();
+END;//
+delimiter ;
+
+DROP TRIGGER IF EXISTS tgg_update_orders;
+delimiter //
+CREATE TRIGGER tgg_update_orders
+BEFORE UPDATE ON orders
+FOR EACH ROW 
+BEGIN
+	SET NEW.modified_date = now();
+END;//
+delimiter ;
+
+INSERT INTO destinations(name, description) VALUES ('Europe', 'Europe is an amazing place for a holiday. From the famous Eiffel Tower and Louvre Museum in Paris to the mountain railway of Jungfrau in Switzerland or Amsterdam’s exciting nightlife,  we can take you to all the best bits of Europe in our modern, comfortable coaches.');
+INSERT INTO destinations(name, description) VALUES ('Asia', 'Sum up Asia in a paragraph? Good luck. Positively massive by just about every measure, the sole thread that unites this diverse continent is the sheer diversity of experiences it presents to travellers.');
+INSERT INTO destinations(name, description) VALUES ('Africa', 'On this route you will experience \'The World in one Country\' with very spectacular scenery, big game on safari, African Cultures, Ocean, Mountains and semi-desert. The menu is very varied and spiced by your guide with background information and stories about present and past.');
+INSERT INTO destinations(name, description) VALUES ('Australia', 'Tourists visit places and see things. Travellers journey to places and experience things. At Aussie Farmstay and Bush Adventures we design our tours for travellers, not tourists. We travel in a small group (maximum of 11 passengers) to ensure the experience is a personalised one. As long as two passengers have booked an Aussie Farmstay and Bush Adventures tour, the tour will proceed.');
+INSERT INTO destinations(name, description) VALUES ('South America', 'Colourful, candid and everything in between, South America beats to the rhythm of an infectious, eternal drum. Captured in the chaos of Rio\'s carnival, echoing on Columbia\'s Caribbean beaches and booming along the peaks of the Andes, an effervescent spirit bursts from every facet of life on this remarkable continent.');
---
+++ index.php
@@ -0,0 +1,173 @@
+<?php
+$title_for_layout = 'Bon Voyage | Travel Agency';
+?>
+
+<html>
+	<?php include './layout/documents.php'; ?>
+
+	<body>
+
+		<div class="div-header col-xs-12">
+			<div class="container">
+				<div class="logo"></div>
+				<h1>BON VOYAGE</h1>
+				<span>Best Prices, Best Holidays</span>
+			</div>
+		</div>
+
+		<div class="div-navigation col-xs-12">
+			<div class="container">
+				<ul>
+					<li><a href="#" class="active">Home</a></li>
+					<li><a href="#">Destinations</a></li>
+					<li><a href="#">Vacations</a></li>
+					<li><a href="#">About Us</a></li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="div-content col-xs-12">
+			<div class="container">
+
+				<div class="home-left-col text-center pull-left">
+					<div class="social-icons">
+						<a href="#"><img src="img/facebook.png" /></a>
+						<a href="#"><img src="img/twitter.png" /></a>
+						<a href="#"><img src="img/googleplus.png" /></a>
+						<a href="#"><img src="img/pinterest.png" /></a>
+					</div>
+					<img src="img/binoculars.png" style="margin-bottom: 18px" />
+					<a href="" class="search">S E A R C H</a>
+					<span>For A Holiday</span>
+					<a href="#" class="btn-custom" style="width: 100%">WELCOME TO OUR WORLD</a>
+				</div>
+
+				<div class="home-right-col pull-right">
+					<p>SEE HOW MUCH <br/>YOU CAN SAVE WITH THESE LAST MINUTE BON VOYAGE<br/> RATES.</p>
+					<a href="#" class="pull-right" style="margin-top: 5px;">See Details &nbsp;&nbsp;<span class="seemore">&plus;</span></a>
+				</div>
+
+				<div class="clearfix"></div>
+
+				<div class="div-space"></div>
+
+				<div class="home-left-col-1 pull-left">
+					<h2>Handpicked Offers</h2>
+					<img class="h2-icon" src="img/clock.png" />
+					<div class="white-content">
+						<span class="list-details">
+							Romantic Paris <span class="color-white">---------------</span> $449
+						</span>
+						<span class="list-details">
+							Sri Lanka <span class="color-white">--------------------</span> $699
+						</span>
+						<span class="list-details">
+							Tuscany to Provence <span class="color-white">----------</span> $845
+						</span>
+						<span class="list-details">
+							Florida Getaway <span class="color-white">--------------</span> $199
+						</span>
+						<span class="list-details">
+							NYC Escape <span class="color-white">------------------</span> $899
+						</span>
+						<a href="#" class="link-seemore">
+							See more &nbsp;<span class="seemore">&plus;</span>
+						</a>
+					</div>
+
+					<h2>Summer Destinations</h2>
+					<img class="h2-icon" src="img/plane.png" />
+					<div class="white-content" style="margin-bottom: 3px;">
+						<span class="list-details">
+							Barcelona <span class="color-white">-------------------</span> $999​
+						</span>
+						<span class="list-details">
+							Anchorage <span class="color-white">------------------</span> $199
+						</span>
+						<span class="list-details">
+							Vancouver <span class="color-white">------------------</span> $159
+						</span>
+						<span class="list-details">
+							Maui <span class="color-white">-----------------------</span> $649
+						</span>
+						<span class="list-details">
+							Cape Cod <span class="color-white">--------------------</span> $599
+						</span>
+						<a href="#" class="link-seemore">
+							See more &nbsp;<span class="seemore">&plus;</span>
+						</a>
+					</div>
+				</div>
+
+				<div class="home-right-col-1 pull-right">
+					<h2>$649</h2>
+					<div class="white-content">
+						<p>Romantic Provence Getaway</p>
+						<a href="#" class="seemore">&plus;</a>
+						<div class="border-bottom"></div>
+					</div>
+
+					<h2>$449</h2>
+					<div class="white-content">
+						<p>8 Nights Alaska Cruise All Inclusive</p>
+						<a href="#" class="seemore">&plus;</a>
+						<div class="border-bottom"></div>
+					</div>
+
+					<h2>$649</h2>
+					<div class="white-content">
+						<p>4 Nights All Inclusive Cancun Resort & Spa</p>
+						<a href="#" class="seemore">&plus;</a>
+						<div class="border-bottom"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="clearfix"></div>
+
+		<div class="div-space"></div>
+
+		<div class="footer">
+			<div class="row-1" >
+				<div class="container">
+					<h2>BOOK YOUR VACATION NOW</h2>
+					<a href="#" class="btn-custom pull-right">GO</a>
+				</div>
+			</div>
+
+			<div class="row-2" >
+				<div class="container">
+					<div class="col-xs-6">
+						<h3>About us</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
+						</p>
+						<br />
+						<p>Copyright &copy; 2014. Bon Voyage Coporation.</p>
+					</div>
+					<div class="col-xs-3">
+						<h3>Contacts</h3>
+						<ul>
+							<li>Tel: 123-456-7890</li>
+							<li>Fax: 123-456-7890</li>
+							<li>info@mysite.com</li>
+							<br />
+							<li>2601 ABC St. San Francisco, CA 64789</li>
+						</ul>
+					</div>
+					<div class="col-xs-3">
+						<h3>Partners</h3>
+						<ul>
+							<li>Google</li>
+							<li>Facebook</li>
+							<li>Apple</li>
+							<li>Microsoft</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</body>
+</html>
