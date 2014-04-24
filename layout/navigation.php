76bfde3ec019f507b6e03f9d3bd06529c4d4aab9
Duy Trần <duytran.bkict@gmail.com>
2014-04-24 11:29:13 +0700

    Update aboutus.php

--- layout/navigation.php
+++ layout/navigation.php
@@ -4,7 +4,7 @@
 			<li><a href="index.php" class="<?php if ($active_tab == 'home') echo 'active'; ?>" >Home</a></li>
 			<li><a href="destinations.php" class="<?php if ($active_tab == 'destinations') echo 'active'; ?>" >Destinations</a></li>
 			<li><a href="vacations.php" class="<?php if ($active_tab == 'vacations') echo 'active'; ?>" >Vacations</a></li>
-			<li><a href="#" class="<?php if ($active_tab == 'about_us') echo 'active'; ?>" >About Us</a></li>
+			<li><a href="aboutus.php" class="<?php if ($active_tab == 'aboutus') echo 'active'; ?>" >About Us</a></li>
 		</ul>
 	</div>
 </div>
